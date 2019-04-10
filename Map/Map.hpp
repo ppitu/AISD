6b8dd1a59931368c4cc43a640157c3fa705f04d6
Piotrek <piotr.chmura.1998@o2.pl>
2019-04-10 19:56:44 +0200

    Drugie dodanie

--- Map/Map.hpp
+++ Map/Map.hpp
@@ -10,7 +10,8 @@ class Map
 {
 	public:	
 
-		void insert(std::pair<K, V> );		
+		void insert(std::pair<K, V> );
+		int size();
 
 	private:
 		class MapElem
@@ -24,14 +25,18 @@ class Map
 
 				bool operator<(const MapElem &) const;
 				bool operator>(const MapElem &) const;
+
+				friend ostream& operator<<(ostream&, MapElem &);
+
 		};
 	
 	public:
 		BST<MapElem> bst_t;
-	
+		typename BST<MapElem>::Iterator begin();
+		typename BST<MapElem>::Iterator end();	
 };
 
-/*funkcjie publiczne klasy Map*************************************************/
+/*funkcje publiczne klasy Map*************************************************/
 
 template <typename K, typename V>
 void Map<K, V>::insert(std::pair<K, V> key_value)
@@ -39,7 +44,25 @@ void Map<K, V>::insert(std::pair<K, V> key_value)
 	bst_t.insert(key_value);
 }
 
-/*funkcjie publiczne klasy MapElem********************************************/
+template <typename K, typename V>
+int Map<K, V>::size()
+{
+	return bst_t.size();
+}
+
+template <typename K, typename V>
+typename BST<typename Map<K, V>::MapElem>::Iterator Map<K, V>::begin()
+{
+	return bst_t.begin();
+}
+
+template <typename K, typename V>
+typename BST<typename Map<K, V>::MapElem>::Iterator Map<K, V>::end()
+{
+	return bst_t.end();
+}
+
+/*funkcje publiczne klasy MapElem********************************************/
 
 template <typename K, typename V>
 Map<K, V>::MapElem::MapElem(const std::pair<K, V> &key_value)
@@ -67,4 +90,11 @@ bool Map<K, V>::MapElem::operator>(const MapElem& elem ) const
 	return this->key_value.first > elem.key_value.first;
 }
 
-#endif //MAP_HPP_
+template <typename K, typename V>
+ostream& operator<<(ostream& out, typename Map<K, V>::MapElem &elem)
+{
+	out << "(" << elem.key_value.first << "," << elem.key_value.second << ")";
+	return out;
+}
+
+#endif //MAP_HPP
--- Map/main.cpp
+++ Map/main.cpp
@@ -5,11 +5,24 @@
 
 using namespace std;
 
+template <typename K, typename V>
+void printSetSizeAndElems(Map<K, V> &m)
+{
+	cout << "Size " << m.size() << endl;
+	for(auto &e : m)
+		std::cout << e ;
+	cout << endl;
+}
+
+
+
 int main()
 {
 	Map<int, string> map_t;
 
 	map_t.insert({1, "first"});
+
+	printSetSizeAndElems(map_t);
 
 	return 0;
 }
